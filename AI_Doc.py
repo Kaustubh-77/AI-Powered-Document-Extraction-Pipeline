fabcfffdcaff7b36f026417f5b41121399c45306
Kaustubh-77 <129765047+Kaustubh-77@users.noreply.github.com>
2026-06-17 10:17:01 +0530

    Application
    V1 of the application

---
+++ AI_Doc.py
@@ -0,0 +1,127 @@
+import streamlit as st
+from pypdf import PdfReader
+from docx import Document
+import pymupdf4llm
+import sys
+import torch
+import torchvision
+import builtins
+builtins.torch = torch
+builtins.torchvision = torchvision
+from transformers import pipeline
+from openai import OpenAI
+ 
+
+st.title("AI-Powered Document Extractor")
+st.caption("Designed to help you process information quickly!")
+ 
+st.subheader(":blue[Guidelines]:")
+st.markdown("""
+- The application supports two mode of information preprocessing; Document (PDF and docx format documents are accepted) and Text.
+- Based on the document/text size you have the choice to choose the amount of words your summary to be, for your easy understanding, min. being 100 and max. being 400 words. 
+""")
+ 
+amount = st.slider("How long you want your summary to be?",100,300)
+st.write(f"-> chosen limit: :green[{amount} words]")
+ 
+def process_information():
+    info_type = st.radio(
+        "What kind of information you would like to summarize?",
+        ["Document", "Text"]
+    )
+
+    information_processed = ""
+
+    if info_type == "Text":
+        info = st.text_area(
+            "Paste your information down below!",
+            placeholder="Paste..."
+        )
+
+        information_processed += info
+
+        if not information_processed.strip():
+            st.error("Currently the text is empty!")
+        else:
+            st.write("Extracted text:")
+            st.write(information_processed)
+
+    elif info_type == "Document":
+        document = st.file_uploader(
+            "Upload your file:",
+            type=["pdf", "docx"]
+        )
+
+        if document is not None:
+            st.markdown(f"""
+                        - Name: :green[{document.name}]
+                        - Type: :green[{document.type}]
+""") 
+            if document is not None and document.type == "application/pdf":
+                st.write("Starting extraction...")
+                reader = PdfReader(document)
+
+                information_processed = ""
+
+                for page in reader.pages:
+                    text = page.extract_text()
+                    if text:
+                        information_processed += text
+
+                if not information_processed.strip():
+                    st.warning("No extractable text found (likely scanned PDF).")
+ 
+                st.write("Extracted PDF text:")
+                st.write(information_processed[:500])
+
+            elif document.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+
+                docx = Document(document)
+
+                text = "\n".join([p.text for p in docx.paragraphs])
+
+                information_processed = text
+
+                st.write("Extracted DOCX text:")
+                st.write(information_processed)
+
+    return information_processed
+
+# st.button("Generate Summary!",on_click=summarise_content())
+def summarise_content():
+    information = process_information()
+
+    # api_key=""
+    # client = OpenAI(api_key=api_key)
+    summary_model = pipeline("text-generation")
+    # model="facebook/bart-large-cnn"
+
+    prompt = f"Summarize the following text and give the best summary stating good insights, if it's a technical document and has codes, showcase all codes and explain what they do:\n\n{information}"
+    summary = summary_model(f"{information[:500]}", max_length=amount, min_length=50, return_full_text=False)
+    st.subheader("Here's the summary!")
+    st.write(summary[0]['generated_text'])
+#     response = client.responses.create(
+#     model="gpt-3.5-turbo",
+#     input=f"""
+#     Summarize the following document.
+    
+#     Document:
+#     {information}
+    
+#     Provide:
+#     1. Executive Summary
+#     2. Key Points
+#     3. Action Items
+#     """
+# )
+
+#     summary = response.output_text
+
+#     # Display in Streamlit:
+
+#     st.subheader("Document Summary")
+#     st.write(summary)
+
+
+
+summarise_content()
